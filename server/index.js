dd850b993f8763090c144b787f4060934edebb86
raj1020 <raj.1020@hotmail.com>
2021-02-26 15:59:48 +1100

    defined the app functional component in React and connected to mongoose in backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import {MONGOURI} from './keys';
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 
 
+//
